c4e37b047f159eec8271274a2fb4bf709fdd766a
ParkWonjin-mj <park.wonjin.mz@mynavi.jp>
2023-05-18 04:25:58 +0000

    mynameout

---
+++ wonjin/mynameout.py
@@ -0,0 +1,18 @@
+# print("hello, world")
+# import numpy as np
+# arr = np.array([1.2,3.4,-0.9,-2.1,100],dtype=np.string_)
+
+# print(arr.shape)
+# print(arr.dtype)
+
+
+# astype -> 다른 형으로 바꿀 수 있다. 
+
+
+import sys 
+
+args = sys.argv
+
+name = args[1]
+
+print("Hello",name,"!",end="")
