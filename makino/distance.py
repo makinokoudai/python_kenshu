e84eb8dc60b3a706497421174caf0c437f9e36d1
makino <makino.kodai.ni@mynavi.jp>
2023-05-19 06:04:16 +0000

    distance.py 初めのコミットです

---
+++ makino/distance.py
@@ -0,0 +1,24 @@
+
+import sys
+
+args = sys.argv
+
+input_place1 = args[1]
+input_place2 = args[2]
+
+distance = 0
+
+
+
+place = {"東京":0.00,"品川":6.78,"新横浜":25.54,"名古屋":342.02 ,"京都":476.31,"新大阪":515.35 }
+
+distance =round(abs(place[input_place1] - place[input_place2]),2)  
+
+print(distance,end="")
+
+
+
+
+
+
+
