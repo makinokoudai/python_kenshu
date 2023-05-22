0c950ed2a0e462a367cb5b275e5ee6cf781a275e
KosukeUehata <uehata.kosuke.dx@mynavi.jp>
2023-05-22 07:26:44 +0000

    add OOP

---
+++ uehata/myfamily.py
@@ -0,0 +1,11 @@
+import sys
+from introfamily import IntroFam
+
+name = sys.argv[1]
+age = sys.argv[2]
+cat_name = sys.argv[3]
+
+intro = IntroFam(name,age,cat_name)
+print(intro.name_out())
+print(intro.age_out())
+print(intro.cat_name_out())
