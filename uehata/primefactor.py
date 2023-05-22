17efe5f3d912e3a6ead8db34deba9dff56e69d91
KosukeUehata <uehata.kosuke.dx@mynavi.jp>
2023-05-22 07:42:13 +0000

    update primefactor.py

--- uehata/primefactor.py
+++ uehata/primefactor.py
@@ -1,17 +1,18 @@
-import sys 
+import sys
+import math
 
 num = int(sys.argv[1])
 
 # 素数チェック用関数
 def prime_check(n):
-    for i in range(2,n//2+1):
+    for i in range(2,int(math.sqrt(n))+1):
         if num % i == 0:
             return False
     return True
 
 # 素因数格納用
 prime_factor = []
-for j in range(2, num//2+1):
+for j in range(2, int(math.sqrt(num))+1):
     if prime_check(j):
             while True:
                 if num % j == 0:
