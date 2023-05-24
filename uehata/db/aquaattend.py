6af7146aa5dff08187f28ab13a279a1f537e8b4a
KosukeUehata <uehata.kosuke.dx@mynavi.jp>
2023-05-24 23:56:11 +0000

    update aquaattend

--- uehata/db/aquaattend.py
+++ uehata/db/aquaattend.py
@@ -7,18 +7,14 @@ dt, adult, child  = sys.argv[1:]
 # datetime型に変換
 dt = date(int(dt[:4]),int(dt[4:6]),int(dt[6:8]))
 
-# 連番
-cnt = 1
 # 同じ日付の記録があるかの確認。あれば全て取得。
 aquarium_log = session.query(Aquarium.entry_date).filter_by(entry_date=dt).all()
-if aquarium_log is not None:
-    for _ in aquarium_log:
-        cnt += 1
+seq = len(aquarium_log) + 1
 
 # 登録用データの作成
 aquarium = Aquarium(
     entry_date = dt,
-    seq = cnt,
+    seq = seq,
     adult = int(adult),
     child = int(child),
 )
